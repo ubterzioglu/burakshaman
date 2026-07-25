283eea8fa51c476ee046165d65ae189c258a2e22
Umut Baris Terzioglu <Umut.Baris-Terzioglu@adesso-insurance-solutions.de>
2026-07-25 21:28:01 +0200

    add partner links to footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -54,6 +54,13 @@ export function SiteFooter({ locale, dict }: { locale: Locale; dict: Dictionary 
             <a href="https://ufuksoynakliyat.com.tr/tuzla-evden-eve-nakliyat" rel="dofollow">
               Tuzla Evden Eve Nakliyat
             </a>
+            <span><a href="https://tekhurdametal.com/istanbul-hurdaci/" rel="dofollow">İstanbul Hurdacı</a> Firması Tek Hurda Metal A.Ş</span>
+            <span><a href="https://tekhurdametal.com/hurda-fiyatlari/" rel="dofollow">Hurda Fiyatları</a> Tek Hurda Metal</span>
+            <span><a href="https://tekhurdametal.com/hurda-demir-fiyatlari/" rel="dofollow">Demir Hurda Fiyatları</a> Tek Hurda Metal</span>
+            <span><a href="https://tekhurdametal.com/beylikduzu-hurdaci/" rel="dofollow">Beylikdüzü Hurdacı</a> Firması Tek Hurda Metal A.Ş</span>
+            <span><a href="https://lionerotik.com/urunler/fetis-urunleri" rel="dofollow">Antalya Sex Shop</a> Lion Erotik</span>
+            <span><a href="https://lionerotik.com/urunler/fetis-urunleri" rel="dofollow">Antalya Erotik Shop</a> Lion Erotik</span>
+            <span><a href="https://lionerotik.com/urunler/fetis-urunleri" rel="dofollow">Antalya Seks Shop</a> Lion Erotik</span>
           </div>
         </div>
       </div>
